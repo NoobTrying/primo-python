eb0446fa5267b91d7b3e367d0afa014863f870ba
NoobTrying <noobcoding@tutamail.com>
2026-09-25 15:21:58 +0200

    Primo commit

---
+++ main.py
@@ -0,0 +1,7 @@
+nome = input("Come ti chiami? ")
+eta = int(input("Quanti anni hai? "))
+
+if eta >= 18:
+    print("Ciao", nome, "sei maggiorenne!")
+else:
+    print("Ciao", nome, "sei minorenne!")
